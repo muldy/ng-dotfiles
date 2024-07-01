093a9800d2f7a70ca515b83db8e8be621b1be50b
muldy <muldy@ThinkPad-X260>
2024-07-01 20:51:47 +0100

    update

--- ubuntu_installs.sh
+++ ubuntu_installs.sh
@@ -1,3 +1,5 @@
+DOTFILES_PATH="~/.config/ng-dotfiles"
+
 #!/bin/bash
 sudo apt install \
 	neovim \
@@ -9,4 +11,18 @@ sudo apt install \
 	-y
 
 
+# set defaults
 sudo update-alternatives --set x-terminal-emulator /usr/bin/tilix.wrapper
+
+
+
+# link config files
+FILES=( #relative to home folder
+  ".gitconfig"
+)
+# Check if the file exists
+if [ ! -f "$FILE_PATH" ]; then
+  echo ln -s ~/"$DOTFILES_PATH"/"$FILE_PATH" ~/"$FILE_PATH" 
+else
+  echo "File $FILE_PATH already exists!"
+fi
