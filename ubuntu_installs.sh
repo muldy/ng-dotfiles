fd1d2e91c487416575ddba0ac2da61e5adaf905a
muldy <muldy@ryzen-linux>
2024-07-03 13:08:02 +0100

    bitwarden client added

--- ubuntu_installs.sh
+++ ubuntu_installs.sh
@@ -32,6 +32,8 @@ fi
 
 # set defaults
 sudo update-alternatives --set x-terminal-emulator /usr/bin/tilix.wrapper
+sudo snap install bitwarden
+sudo snap connect bitwarden:password-manager-service
 
 
 echo -e "\nInstalling Oh My ZShell\n"
