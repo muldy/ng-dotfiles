c8ac1caaa0695cf8ee814579de220bda86a044aa
muldy <muldy@ryzen-linux>
2024-07-03 17:11:36 +0100

    added fixes script

---
+++ fixes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# tracker-3 disable:
+systemctl --user mask tracker-extract-3.service tracker-miner-fs-3.service tracker-miner-rss-3.service tracker-writeback-3.service tracker-xdg-portal-3.service tracker-miner-fs-control-3.service
+tracker3 reset -s -r
