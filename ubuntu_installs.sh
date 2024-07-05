c0b99bb03e4df6f7436ddc1ebc6db228f5417e00
muldy <muldy@ryzen-linux>
2024-07-05 17:29:31 +0100

    fix

--- ubuntu_installs.sh
+++ ubuntu_installs.sh
@@ -35,7 +35,7 @@ fi
 sudo update-alternatives --set x-terminal-emulator /usr/bin/tilix.wrapper
 sudo snap install bitwarden
 sudo snap connect bitwarden:password-manager-service
-dconf load /com/gexperts/Terminix/ < terminix.dconf
+dconf load /com/gexperts/Tilix/ < Tilix.dconf
 
 
 echo -e "\nInstalling Oh My ZShell\n"
