9a7e1eecf4cec1ee0fe4932be7279bb71fee9835
muldy <muldy@ryzen-linux>
2024-07-05 18:42:05 +0100

    fix

--- ubuntu_installs.sh
+++ ubuntu_installs.sh
@@ -34,12 +34,12 @@ else
   echo -e "\nInstalling Oh My Zsh"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   mv $HOME/.oh-my-zsh $OMZSH_DIR
-  curl -L -o $DF_FONTS_FOLDER/fonts.tar.zx \
-	  https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Ubuntu.tar.xz
-  tar -xvJf \
-	  $DF_FONTS_FOLDER/fonts.tar.zx \
-	  -C $DF_FONTS_FOLDER
-  rm -rf $DF_FONTS_FOLDER/fonts.tar.zx
-  fc-cache -f -v
 fi
 
+curl -L -o $DF_FONTS_FOLDER/fonts.tar.zx \
+	https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Ubuntu.tar.xz
+tar -xvJf \
+	$DF_FONTS_FOLDER/fonts.tar.zx \
+	-C $DF_FONTS_FOLDER
+rm -rf $DF_FONTS_FOLDER/fonts.tar.zx
+fc-cache -f -v
