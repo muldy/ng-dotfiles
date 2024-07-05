61c987903e1ce1e8763b47c5a3cb8cf95bd78e77
muldy <muldy@ryzen-linux>
2024-07-05 10:54:56 +0100

    added lvim packages

--- lvim.sh
+++ lvim.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -u 
+echo -e "\nInstalling packages\n"
+sudo apt install \
+	build-essential \
+	flake8 \
+	tree-sitter-cli \
+	fd-find \
+	ripgrep \ 
+	-y
+
+sudo systemctl daemon-reload
+
 
 LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
