d7995c70f86508dd901d9473853486cbcd8bd5ad
muldy <muldy@ryzen-linux>
2024-07-06 11:35:20 +0100

    added assets, fix

--- ubuntu_wayland_addons.sh
+++ ubuntu_wayland_addons.sh
@@ -5,7 +5,7 @@ sudo apt install -y\
 	gitk \
 	tilix \
 	gnome-shell-extension-manager \
-	variety \
+	variety 
 
 # set defaults
 sudo update-alternatives --set x-terminal-emulator /usr/bin/tilix.wrapper
@@ -13,10 +13,10 @@ sudo snap install bitwarden
 sudo snap connect bitwarden:password-manager-service
 
 echo -e "Adding fonts"
-curl -L -o $DF_FONTS_FOLDER/fonts.tar.zx \
+curl -L -o "$DF_FONTS_FOLDER/fonts.tar.zx" \
 	https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Ubuntu.tar.xz
 tar -xvJf \
-	$DF_FONTS_FOLDER/fonts.tar.zx \
-	-C $DF_FONTS_FOLDER
-rm -rf $DF_FONTS_FOLDER/fonts.tar.zx
+	"$DF_FONTS_FOLDER/fonts.tar.zx" \
+	-C "$DF_FONTS_FOLDER"
+rm -rf "$DF_FONTS_FOLDER/fonts.tar.zx"
 fc-cache -f -v
