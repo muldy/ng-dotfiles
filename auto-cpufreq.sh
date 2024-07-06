7eac09ea252cea786c3b95dcf9193fb42d7b3e80
muldy <muldy@ryzen-linux>
2024-07-06 12:04:31 +0100

    tweak

--- auto-cpufreq.sh
+++ auto-cpufreq.sh
@@ -2,8 +2,9 @@
 set -e 
 
 if [ -d "/sys/class/power_supply/BAT0" ]; then
-    git clone https://github.com/AdnanHodzic/auto-cpufreq.git $HOME/git/auto-cpufreq
-    cd $HOME/git/auto-cpufreq && sudo ./auto-cpufreq-installer
+    git clone https://github.com/AdnanHodzic/auto-cpufreq.git "$HOME/git/auto-cpufreq"
+    cd "$HOME/git/auto-cpufreq && sudo ./auto-cpufreq-installer"
+    sudo cp auto-cpufreq.conf /etc/
 else
   echo -e "\nNo Battery detected"
 fi
