763bf4c243566fd2f2c97fe5cd01c62dfa405b27
muldy <muldy@ryzen-linux>
2024-07-07 10:48:29 +0100

    updated homebrew

--- homebrew.sh
+++ homebrew.sh
@@ -1,7 +1,14 @@
-#!bin/bash
-
+#!/usr/bin/env bash
 set -e 
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install k9s kubectx kubens
+export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
+
+brew install \
+  k9s \
+  kubectx \
+  helm \
+  kustomize \
+  terraform 
+
--- zshrc
+++ zshrc
@@ -2,6 +2,9 @@
 # export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
 export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
 
+unset HOMEBREW_NO_AUTO_UPDATE=1
+unset HOMEBREW_NO_INSTALL_CLEANUP
+
 #Note that the directories 
 #
 #'/var/lib/flatpak/exports/share'
