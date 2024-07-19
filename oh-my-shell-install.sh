c1148d36448fe893c012f7df25fd883ffb6f1c4b
muldy <muldy@ryzen-linux>
2024-07-19 20:27:22 +0100

    update

--- oh-my-shell-install.sh
+++ oh-my-shell-install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e 
 
+rm -rf "$HOME/.oh-my-zsh" "$HOME/.config/oh-my-zsh"
+
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -o /tmp/install.sh
 
-sh /tmp/install.sh
+echo "sh /tmp/install.sh"
 
