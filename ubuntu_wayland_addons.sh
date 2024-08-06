f4441b2a8e58aab41c83ee6b69a111a8a9f39278
muldy <muldy@ryzen-linux>
2024-08-06 12:57:31 +0100

    install kitty

--- ubuntu_wayland_addons.sh
+++ ubuntu_wayland_addons.sh
@@ -4,12 +4,13 @@ set -e
 sudo apt install -y\
 	gitk \
 	tilix \
+  kitty \
 	gnome-shell-extension-manager \
   gpaste-2 \
 	variety 
 
 # set defaults
-sudo update-alternatives --set x-terminal-emulator /usr/bin/tilix.wrapper
+sudo update-alternatives --set x-terminal-emulator /usr/bin/tilix.wrapper  
 sudo snap install obsidian 
 sudo snap install bitwarden
 sudo snap connect bitwarden:password-manager-service
